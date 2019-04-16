abccc77cdf11da8fd155252721a27d375a1f316d
Brent Robertson <brobertson@doctorgenius.com>
2019-04-15 19:00:11 -0700

    Change to the layout for title and split screen blocks on the homepage.
    Followed the component structure as the about page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -46,76 +46,73 @@ export default ({ data }, props) => (
       </div>
       <Main>
         <Container>
-          <div class="row">
-            <div class="col-xl-12">
-              <div class="header inner-title">
-                <h3 class="strapline">Performance, Results, Excellence</h3>
-                <h2 class="underline">We Care About Your Success</h2>
+          <div className="row row-medium">
+            <div className="col-xl-12 d-flex align-items-center justify-content-center">
+              <div className="header inner-title">
+                <h3 className="strapline">Performance, Results, Excellence</h3>
+                <h2 className="underline">We Care About Your Success</h2>
               </div>
             </div>
-            <div class="col-xl-3">
-              <div class="panel">
-                <img src={medal} alt="" />
-                <div class="inner-title">
-                  <h2>1,125%</h2>
+            <div className="col-xl-3 align-self-start">
+              <div className="panel d-flex flex-sm-column align-items-center justify-content-start">
+                <div class="icon-wrapper">
+                  <img className="img-icon" src={medal} alt="Icon of a medal" />
                 </div>
+                <p class="panel-heading">1,125%</p>
                 <p>Increase in Google First-Page Results</p>
               </div>
             </div>
-            <div class="col-xl-3">
-              <div class="panel">
-                <img src={money} alt="" />
-                <div class="inner-title">
-                  <h2>100%</h2>
+            <div className="col-xl-3 align-self-start">
+              <div className="panel d-flex flex-sm-column align-items-center justify-content-start">
+                <div class="icon-wrapper">
+                  <img className="img-icon" src={money} alt="Icon of money" />
                 </div>
+                <p class="panel-heading">100%</p>
                 <p>Increase in Revenue!</p>
               </div>
             </div>
-            <div class="col-xl-3">
-              <div class="panel">
-                <img src={groupChat} alt="" />
-                <div class="inner-title">
-                  <h2>88%</h2>
+            <div class="col-xl-3 align-self-start">
+              <div class="panel d-flex flex-sm-column align-items-center justify-content-start">
+                <div class="icon-wrapper">
+                  <img
+                    className="img-icon"
+                    src={groupChat}
+                    alt="Icon of a Groupchat"
+                  />
                 </div>
+                <p class="panel-heading">88%</p>
                 <p>Increase in New Patient Calls!</p>
               </div>
             </div>
-            <div class="col-xl-3">
-              <div class="panel">
-                <img src={stars} alt="" />
-                <div class="inner-title">
-                  <h2>4+ Stars</h2>
+            <div class="col-xl-3 align-self-start">
+              <div class="panel d-flex flex-sm-column align-items-center justify-content-start">
+                <div class="icon-wrapper">
+                  <img className="img-icon" src={stars} alt="Icon of Stars" />
                 </div>
+                <p class="panel-heading">4+ Stars</p>
                 <p>We take pride in our Google reviews</p>
               </div>
             </div>
           </div>
-          <div class="row">
+          <div class="row row-medium d-flex align-items-center justify-content-center">
             <div class="col-xl-12">
-              <div class="content-block">
-                <div class="row">
-                  <div class="col-xl-12">
-                    <div class="header inner-title">
-                      <h3 class="strapline">Built for success</h3>
-                      <h2>
-                        All-in-One <span class="bold">Marketing Dashboard</span>
-                      </h2>
-                    </div>
-                  </div>
-                  <div class="col-xl-12">
-                    <p class="accented">
-                      Our robust, comprehensive marketing solutions helps keep
-                      your online digital marketing presence ahead of
-                      competition. We provide tools and analysis to improve new
-                      patient acquisition with transparent reporting to measure
-                      our results.
-                    </p>
-                  </div>
-                </div>
+              <div class="header inner-title">
+                <h3 class="strapline">Built for success</h3>
+                <h2 class="underline">
+                  All-in-One <span class="bold">Marketing Dashboard</span>
+                </h2>
               </div>
             </div>
+            <div class="accented-paragraph col-xl-6">
+              <p>
+                Our robust, comprehensive marketing solutions helps keep your
+                online digital marketing presence ahead of competition. We
+                provide tools and analysis to improve new patient acquisition
+                with transparent reporting to measure our results.
+              </p>
+            </div>
           </div>
-          <div class="row">
+          <div class="row row-tall">
             <div class="col-xl-12">
               <div class="full-bleed marketing-dashboard-background d-flex justify-content-center align-items-center">
                 <img
@@ -126,11 +123,11 @@ export default ({ data }, props) => (
               </div>
             </div>
           </div>
-          <div class="row">
+          <div class="row row-medium d-flex align-items-center">
             <div class="col-xl-12">
               <div class="header inner-title">
                 <h3 class="strapline">Setting the foundation for success</h3>
-                <h2>
+                <h2 class="underline">
                   Why Choose <span class="bold">Doctor Genius</span>
                 </h2>
               </div>
@@ -139,55 +136,73 @@ export default ({ data }, props) => (
               <img src={newPatientAcquisitions} alt="" />
             </div>
             <div class="col-xl-6 d-flex justify-content-center align-items-center">
-              <div class="inner-title">
-                <h2>Why is Doctor Genius #1 in new patient acquisitions?</h2>
-                <p class="accented">
-                  When you join the Doctor Genius Family, we are now partnered.
-                  We have a direct interest in your business doing well.
-                </p>
+              <div className="content-block">
+                <div class="inner-title">
+                  <h2>Why is Doctor Genius #1 in new patient acquisitions?</h2>
+                  <div class="accented-paragraph">
+                    <p>
+                      When you join the Doctor Genius Family, we are now
+                      partnered. We have a direct interest in your business
+                      doing well.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div class="row">
+          <div class="row row-medium">
             <div class="col-xl-6 d-flex justify-content-center align-items-center">
-              <div class="inner-title">
-                <h2>What makes Doctor Genius different?</h2>
-                <p class="accented">
-                  We are not a cookie cutter type program. We are a performance
-                  based program and take pride in your success.
-                </p>
+              <div class="content-block">
+                <div class="inner-title">
+                  <h2>What makes Doctor Genius different?</h2>
+                  <div class="accented-paragraph">
+                    <p>
+                      We are not a cookie cutter type program. We are a
+                      performance based program and take pride in your success.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="col-xl-6">
               <img src={dgDifferent} alt="" />
             </div>
           </div>
-          <div class="row">
+          <div class="row row-medium">
             <div class="col-xl-6">
               <img src={dgClientSatisfaction} alt="" />
             </div>
             <div class="col-xl-6 d-flex justify-content-center align-items-center">
-              <div class="inner-title">
-                <h2>How would you rate Doctor Genius client satisfaction?</h2>
-                <p class="accented">
-                  Don't take our word for it, see what our clients say about us
-                  100+ 5 Stars. Check our <a href="/reviews">reviews</a>
-                </p>
+              <div class="content-block">
+                <div class="inner-title">
+                  <h2>How would you rate Doctor Genius client satisfaction?</h2>
+                  <div class="accented-paragraph">
+                    <p>
+                      Don't take our word for it, see what our clients say about
+                      us 100+ 5 Stars. Check our <a href="/reviews">reviews</a>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-xl-6">
-              <div class="inner-title">
-                <h2>
-                  Start your <span class="bold">success</span> with Doctor
-                  Genius today!
-                </h2>
-                <p class="accented">
-                  The Genius platform makes every part of your company process
-                  more efficient. Our support team is very excited to help you
-                  and get your company on the right path of success.
-                </p>
+          <div class="row row-medium">
+            <div class="col-xl-6 d-flex justify-content-center align-items-center">
+              <div class="content-block">
+                <div class="inner-title">
+                  <h2>
+                    Start your <span class="bold">success</span> with Doctor
+                    Genius today!
+                  </h2>
+                  <div class="accented-paragraph">
+                    <p class="accented-paragraph">
+                      The Genius platform makes every part of your company
+                      process more efficient. Our support team is very excited
+                      to help you and get your company on the right path of
+                      success.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
             <div class="col-xl-6 d-flex justify-content-center align-items-center">
