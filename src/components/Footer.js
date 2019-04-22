d107b6119aad8c1eabec2dd48fdb1f7c63a4a160
Brent Robertson <brobertson@doctorgenius.com>
2019-04-22 14:29:21 -0700

    Implementing the sitewide footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,107 @@
 import React from "react"
+import { Link } from "gatsby"
+import Container from "../components/Container"
+
+const ListLink = props => (
+  <li>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
 
 export default ({ children }) => (
-    <div>
-        <p>this is the footer</p>
-        {children}
-    </div>
+  <footer>
+    <Container>
+      <div className="row padded tall-top footer-row">
+        <div className="col-sm-4">
+          <img src="/icon/logo-white.svg" />
+        </div>
+        <div className="col-sm-4 footer-page-links">
+          <ul>
+            <ListLink to="/">Home</ListLink>
+            <ListLink to="/marketing-solutions/">Marketing Solutions</ListLink>
+            <li>Who We Serve</li>
+            <ListLink to="/plans/">Plans</ListLink>
+            <ListLink to="/company/">Company</ListLink>
+            <ListLink to="/study-archive/">Resources</ListLink>
+            <li>Free Demo</li>
+            <ListLink to="/contact/">Contact</ListLink>
+          </ul>
+        </div>
+        <div className="col-sm-4">
+          <div className="footer-contact-row">
+            <div className="icon-wrapper">
+              <img className="img-icon" src="/icon/ringing-phone.svg" />
+            </div>
+            <p className="footer-contact-details">&#40;877&#41; 477-2311</p>
+          </div>
+          <div className="footer-contact-row">
+            <div className="icon-wrapper">
+              <img className="img-icon" src="/icon/mail.svg" />
+            </div>
+            <p className="footer-contact-details">support@doctorgenius.com</p>
+          </div>
+          <div className="footer-contact-row">
+            <div className="icon-wrapper">
+              <img className="img-icon" src="/icon/map-pin.svg" />
+            </div>
+            <address className="footer-contact-details">
+              2121 Alton Parkway
+              <br />
+              Irvine, CA 92880
+            </address>
+          </div>
+          <div class="social-media-row">
+            <div class="social-media-item">
+              <a
+                href="https://www.facebook.com/DoctorGeniusMarketing"
+                target="_blank"
+              >
+                <img src="/icon/facebook-circle.svg" />
+              </a>
+            </div>
+            <div class="social-media-item">
+              <a href="https://twitter.com/DoctorGeniusCA" target="_blank">
+                <img src="/icon/twitter-circle.svg" />
+              </a>
+            </div>
+            <div class="social-media-item">
+              <a
+                href="https://www.instagram.com/doctor.genius/"
+                target="_blank"
+              >
+                <img src="/icon/instagram-circle.svg" />
+              </a>
+            </div>
+            <div class="social-media-item">
+              <a
+                href="https://www.linkedin.com/company/doctor-genius"
+                target="_blank"
+              >
+                <img src="/icon/linkedin-circle.svg" />
+              </a>
+            </div>
+            <div class="social-media-item">
+              <a
+                href="https://www.youtube.com/channel/UCEOt77NoRiRrQzDgjpQwDCA/videos"
+                target="_blank"
+              >
+                <img src="/icon/youtube-circle.svg" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row padded short-top footer-subrow">
+        <div class="copyright-section">
+          &copy;2019 Doctor Genius. All Rights Reserved.
+        </div>
+        <div class="legal-links">
+          <ListLink to="/">Privacy Policy</ListLink>
+          <span> | </span>
+          <ListLink to="/">Terms and Conditions</ListLink>
+        </div>
+      </div>
+      {children}
+    </Container>
+  </footer>
 )
