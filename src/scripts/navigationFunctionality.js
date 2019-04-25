d4bb2a2eea83a95374c903c15d000b45b9ac1780
Brent Robertson <brobertson@doctorgenius.com>
2019-04-25 11:55:13 -0700

    Made the navbar shrink/grow transition more fluid

--- src/scripts/navigationFunctionality.js
+++ src/scripts/navigationFunctionality.js
@@ -10,11 +10,18 @@ import phoneBlue from "../img/phone-blue.svg"
       // const height =
       //   document.documentElement.scrollHeight -
       //   document.documentElement.clientHeight
-
       // const scrolled = winScroll / height
-
-      if (winScroll > 0) {
+      if (winScroll >= 100 && winScroll < 200) {
         document.querySelector("nav.navbar").classList.add("main-navbar-stuck")
+        document.querySelector("nav.navbar").classList.add("stuck-midway")
+        document.querySelector("nav.navbar").classList.remove("stuck-fully")
+        document.querySelector("nav.navbar .navbar-brand").src =
+          "../icon/logo-blue.svg"
+        document.querySelector("nav.navbar .phone").src = phoneBlue
+      } else if (winScroll >= 200) {
+        document.querySelector("nav.navbar").classList.add("main-navbar-stuck")
+        document.querySelector("nav.navbar").classList.add("stuck-fully")
+        document.querySelector("nav.navbar").classList.remove("stuck-midway")
         document.querySelector("nav.navbar .navbar-brand").src =
           "../icon/logo-blue.svg"
         document.querySelector("nav.navbar .phone").src = phoneBlue
@@ -22,6 +29,7 @@ import phoneBlue from "../img/phone-blue.svg"
         document
           .querySelector("nav.navbar")
           .classList.remove("main-navbar-stuck")
+        document.querySelector("nav.navbar").classList.remove("stuck-midway")
         document.querySelector("nav.navbar .navbar-brand").src =
           "../icon/logo-white.svg"
         document.querySelector("nav.navbar .phone").src = phoneWhite
