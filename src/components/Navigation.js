56b49dadfc4bcf6dd7191ca20fb0d0078549756b
Brent Robertson <brobertson@doctorgenius.com>
2019-04-30 11:45:11 -0700

    Adding dropdown menus and click event handler to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,15 +4,315 @@ import Container from "../components/Container"
 
 import logo from "../img/logo-white.svg"
 import phone from "../img/phone-white.svg"
+import ourTech from "../../public/icon/our-technology-white.svg"
+import responsiveWebsites from "../../public/icon/website-white.svg"
+import info from "../../public/icon/info.svg"
+import contentMarketing from "../../public/icon/contract-white.svg"
+import onlineReputation from "../../public/icon/online-reputation-white.svg"
+import digitalAdvertising from "../../public/icon/digital-advertising-white.svg"
+import hostingSolutions from "../../public/icon/hosting-solutions-white.svg"
+import dentalPractices from "../../public/icon/tooth.svg"
+import cosmeticSurgeons from "../../public/icon/surgeon.svg"
+import urgentCare from "../../public/icon/urgent-care.svg"
+import healthcareMarketing from "../../public/icon/sphygmomanometer.svg"
+import caseStudy from "../../public/icon/briefcase.svg"
+import webinars from "../../public/icon/television.svg"
+import theStudy from "../../public/icon/the-study-white.svg"
 
 import "../styles/global-styles.scss"
-// import "../scripts/navigationFunctionality.js"
+import "bootstrap/dist/js/bootstrap.min.js"
+// import "../../static/scripts/navigationFunctionality.js"
 
 const ListLink = props => (
   <li className={props.className} data-toggle={props.dataToggle}>
     <Link to={props.to} activeClassName="active" partiallyActive={true}>
       {props.children}
     </Link>
+    {}
+  </li>
+)
+
+const OurClientsListLink = props => (
+  <li className={props.dataToggle}>
+    <Link
+      className=""
+      to={props.to}
+      activeClassName="active"
+      partiallyActive={true}
+      data-toggle={props.dataToggle}
+    >
+      {props.children}
+    </Link>
+    <div className="dropdown-menu">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={dentalPractices} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Dental Practices
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <img className="" src={cosmeticSurgeons} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Cosmetic Surgeons
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={urgentCare} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Urgent Care Clinics
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <img className="" src={healthcareMarketing} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Healthcare Marketing
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={ourTech} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Chiropractor Practices
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to} />
+                <p />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {}
+  </li>
+)
+
+const MarketingSolutionsListLink = props => (
+  <li className={props.dataToggle}>
+    <Link
+      className=""
+      to={props.to}
+      activeClassName="active"
+      partiallyActive={true}
+      data-toggle={props.dataToggle}
+    >
+      {props.children}
+    </Link>
+    <div className="dropdown-menu">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={responsiveWebsites} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Responsive Websites
+                </Link>
+                <p>
+                  Our websites are power-packed with fresh & unique content
+                  tailored to your practice.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <img className="" src={onlineReputation} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Online Reputation
+                </Link>
+                <p>
+                  Our websites are power-packed with fresh & unique content
+                  tailored to your practice.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={contentMarketing} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Content Marketing
+                </Link>
+                <p>
+                  Our websites are power-packed with fresh & unique content
+                  tailored to your practice.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <img className="" src={ourTech} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Our Technology
+                </Link>
+                <p>
+                  Our websites are power-packed with fresh & unique content
+                  tailored to your practice.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={digitalAdvertising} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Digital Advertising
+                </Link>
+                <p>
+                  Our websites are power-packed with fresh & unique content
+                  tailored to your practice.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <img className="" src={hostingSolutions} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Hosting Solutions
+                </Link>
+                <p>
+                  Our websites are power-packed with fresh & unique content
+                  tailored to your practice.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    {}
+  </li>
+)
+
+const StudySingleListLink = props => (
+  <li className={props.dataToggle}>
+    <Link
+      className=""
+      to={props.to}
+      activeClassName="active"
+      partiallyActive={true}
+      data-toggle={props.dataToggle}
+    >
+      {props.children}
+    </Link>
+    <div className="dropdown-menu">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={theStudy} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  The Study
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <img className="" src={webinars} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Webinars
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={caseStudy} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Case Studies
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <img className="" src={info} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  Knowledge Base
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row dropdown-row">
+            <div className="col-sm-6">
+              <img className="" src={ourTech} />
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to}>
+                  eBooks &amp; Whitepapers
+                </Link>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                  volutpat mi sit amet viverra posuere.
+                </p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="dropdown-content-block">
+                <Link className="" to={props.to} />
+                <p />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     {}
   </li>
 )
@@ -31,20 +331,20 @@ export default ({ children }) => (
         id="navbarSupportedContent"
       >
         <ul className="navbar-nav">
-          <ListLink
-            className="dropdown-marketing-solutions"
-            dataToggle="dropdown-marketing-solutions"
+          <MarketingSolutionsListLink
+            className="nav-item dropdown"
+            dataToggle="dropdown"
             to="/marketing-solutions/"
           >
             Marketing Solutions
-          </ListLink>
-          <ListLink
-            className="dropdown-our-clients"
-            dataToggle="dropdown-our-clients"
+          </MarketingSolutionsListLink>
+          <OurClientsListLink
+            className="nav-item dropdown"
+            dataToggle="dropdown"
             to="/our-clients/"
           >
             Who We Serve
-          </ListLink>
+          </OurClientsListLink>
 
           <ListLink className="" to="/plans/">
             Plans
@@ -52,13 +352,13 @@ export default ({ children }) => (
           <ListLink className="" to="/company/">
             Company
           </ListLink>
-          <ListLink
-            className="dropdown-the-study"
-            dataToggle="dropdown-the-study"
+          <StudySingleListLink
+            className="nav-item dropdown"
+            dataToggle="dropdown"
             to="/the-study/"
           >
             Resources
-          </ListLink>
+          </StudySingleListLink>
         </ul>
       </div>
       <div class="contact-links">
