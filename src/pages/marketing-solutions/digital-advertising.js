714ec5e5a65e30418ab37240808707ec1affaf8f
dg-jparmenter <31138530+dg-jparmenter@users.noreply.github.com>
2019-04-30 18:15:08 -0700

    Fleshing out marketing solutions (first) inner page, implemented w template

--- src/pages/marketing-solutions/digital-advertising.js
+++ src/pages/marketing-solutions/digital-advertising.js
@@ -1,10 +1,109 @@
 import React from "react"
 
-import DefaultPageLayout from "../../components/DefaultPageLayout"
-
+import MarketingSolutionsTemplate from "../../templates/marketing-solutions-inner"
 
 export default () => (
-  <DefaultPageLayout>
-    <h1>Digital Advertising</h1>
-  </DefaultPageLayout>
+  <MarketingSolutionsTemplate
+    page="/marketing-solutions/digital-advertising/" /* this should be dynamic from the page props */
+    contentRows={[
+      {
+        content: {
+          icon: {
+            url: "/icon/website.svg",
+            alt: "websites",
+            title: "Websites",
+          },
+          innerTitle: "Design Your Presence.",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+        },
+        image: {
+          url: "/png/design-your-presence.png",
+          alt: "Website with Phone Version",
+        },
+      },
+      {
+        content: {
+          icon: {
+            url: "/icon/fingerprint.svg",
+            alt: "Brand Fingerprint",
+            title: "Brand Identity",
+          },
+          innerTitle:
+            "Your brand is the most precious component of your marketing.",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+        },
+        image: {
+          url: "/png/your-brand.png",
+          alt: "Designers Raising a Brand Sign",
+        },
+      },
+      {
+        content: {
+          icon: {
+            url: "/icon/smartphone.svg",
+            alt: "Mobile Device",
+            title: "Mobile First",
+          },
+          innerTitle: "Design & developing for maximum impact.",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+        },
+        image: {
+          url: "/png/design-for-maximum-impact.png",
+          alt: "Website Displayed on a Mobile Phone",
+        },
+      },
+      {
+        content: {
+          icon: {
+            url: "/icon/speedometer.svg",
+            alt: "A Speedometer",
+            title: "Speed",
+          },
+          innerTitle:
+            "We build websites that will perform your business ranking.",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+        },
+        image: {
+          url: "/png/lighthouse-result.png",
+          alt: "High-Scoring Speed Test Results for a Website",
+        },
+      },
+      {
+        content: {
+          icon: {
+            url: "/icon/awda.svg",
+            alt: "AwDA Compliance Checkmark List",
+            title: "AwDA Compliant",
+          },
+          innerTitle: "Leave no one behind. AwDa compliant.",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+        },
+        image: {
+          url: "/png/awda-accessibility.png",
+          alt: "Accessible Website",
+        },
+      },
+      {
+        content: {
+          icon: {
+            url: "/icon/search-engine.svg",
+            alt: "Online Search Engine",
+            title: "Search Engine Optimization",
+          },
+          innerTitle: "Identify your patients and valuable keywords.",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+        },
+        image: {
+          url: "/png/search-result.png",
+          alt: "Search Engine Results with Your Practice In #1",
+        },
+      },
+    ]}
+  />
 )
---
+++ src/templates/marketing-solutions-inner.js
@@ -0,0 +1,218 @@
+import React from "react"
+
+import DefaultPageLayout from "../components/DefaultPageLayout"
+import Main from "../components/main-content"
+import Container from "../components/Container"
+
+import contentMarketingIcon from "../img/contract.svg"
+import responsiveWebsitesIcon from "../img/website.svg"
+import digitalAdvertisingIcon from "../img/digital-advertising.svg"
+import onlineReputationIcon from "../img/online-reputation.svg"
+import ourTechnologyIcon from "../img/our-technology.svg"
+import hostingSolutionsIcon from "../img/hosting-solutions.svg"
+import lighthouseResult from "../img/lighthouse-result.png"
+
+import "../styles/marketing-solutions-inner.scss"
+import "../styles/global-styles.scss"
+
+export default props => {
+  /* These will likely need to be dynamic props */
+  const pageTitle = "Responsive Websites"
+  const pageSubtitle = "Design that leaves a lasting impression."
+  const pageIntro =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Lorem ipsum dolor sit amet, con."
+
+  // For now all inner pages will have the same hero image, so this will be in the sass
+  //const heroImage = "/hero/marketing-solutions-subpage.png"
+
+  const contentRows = props.contentRows
+  const currentPage = props.page
+
+  return (
+    <DefaultPageLayout>
+      <div className={props["*"] + " marketing-solutions-inner"}>
+        <div className="hero">
+          {/* Hero will be a layout component */}
+          {/* @todo: Heros need a thin grey border underneath, not included in pic */}
+          <Container>
+            <div className="valign-wrapper row">
+              <div className="col-sm-12">
+                <div class="hero-content accent-block">
+                  <h1>{pageTitle}</h1>
+                  <h2>{pageSubtitle}</h2>
+                  <div className="accented-paragraph">
+                    <p>{pageIntro}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+        <Main>
+          <Container>
+            {/* Begin secondary nav Component: */}
+            <div className="row">
+              <div className="col-sm-12 panel-row">
+                <div className="secondary-nav">
+                  <div
+                    className={
+                      (currentPage === "/marketing-solutions/websites/"
+                        ? "active "
+                        : "") + "panel"
+                    }
+                  >
+                    <div className="icon-wrapper">
+                      <a href="/marketing-solutions/websites/">
+                        <img
+                          className="img-icon"
+                          src={responsiveWebsitesIcon}
+                          alt="Responsive Websites"
+                        />
+                      </a>
+                    </div>
+                    <p>Responsive Websites</p>
+                  </div>
+                  <div
+                    className={
+                      (currentPage === "/marketing-solutions/content-marketing/"
+                        ? "active "
+                        : "") + "panel"
+                    }
+                  >
+                    <div className="icon-wrapper">
+                      <a href="/marketing-solutions/content-marketing/">
+                        <img
+                          className="img-icon"
+                          src={contentMarketingIcon}
+                          alt="Content Marketing"
+                        />
+                      </a>
+                    </div>
+                    <p>Content Marketing</p>
+                  </div>
+                  <div
+                    className={
+                      (currentPage ===
+                      "/marketing-solutions/digital-advertising/"
+                        ? "active "
+                        : "") + "panel"
+                    }
+                  >
+                    <div className="icon-wrapper">
+                      <a href="/marketing-solutions/digital-advertising/">
+                        <img
+                          className="img-icon"
+                          src={digitalAdvertisingIcon}
+                          alt="Digital Advertising"
+                        />
+                      </a>
+                    </div>
+                    <p>Digital Advertising</p>
+                  </div>
+                  <div
+                    className={
+                      (currentPage ===
+                      "/marketing-solutions/online-reputation-management/"
+                        ? "active "
+                        : "") + "panel"
+                    }
+                  >
+                    <div className="icon-wrapper">
+                      <a href="/marketing-solutions/online-reputation-management/">
+                        <img
+                          className="img-icon"
+                          src={onlineReputationIcon}
+                          alt="Online Reputation"
+                        />
+                      </a>
+                    </div>
+                    <p>Online Reputation</p>
+                  </div>
+                  <div
+                    className={
+                      (currentPage === "/marketing-solutions/our-technology/"
+                        ? "active "
+                        : "") + "panel"
+                    }
+                  >
+                    <div className="icon-wrapper">
+                      <a href="/marketing-solutions/our-technology/">
+                        <img
+                          className="img-icon"
+                          src={ourTechnologyIcon}
+                          alt="Our Technology"
+                        />
+                      </a>
+                    </div>
+                    <p>Our Technology</p>
+                  </div>
+                  <div
+                    className={
+                      (currentPage === "/marketing-solutions/hosting-solutions/"
+                        ? "active "
+                        : "") + "panel"
+                    }
+                  >
+                    <div className="icon-wrapper">
+                      <a href="/marketing-solutions/hosting-solutions/">
+                        <img
+                          className="img-icon"
+                          src={hostingSolutionsIcon}
+                          alt="Hosting Solutions"
+                        />
+                      </a>
+                    </div>
+                    <p>Hosting Solutions</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* End secondary nav Component */}
+            {/* Begin Split Row of content and image */}
+            {contentRows.map((row, index) => (
+              <div
+                class={
+                  (index % 2 ? "even-row" : "odd-row") +
+                  " row padded short-top short-bottom"
+                }
+              >
+                <div
+                  className={(index % 2 ? "order-sm-12" : "") + " col-sm-1"}
+                />
+                <div class={(index % 2 ? "order-sm-6" : "") + " col-sm-5"}>
+                  <div class="content-block">
+                    <div class="icon-strapline">
+                      <img
+                        src={row.content.icon.url}
+                        class="link-icon"
+                        alt={row.content.icon.alt}
+                      />
+                      <p>{row.content.icon.title}</p>
+                    </div>
+                    <div class="inner-title">
+                      <h2>{row.content.innerTitle}</h2>
+                    </div>
+                    <div class="accented-paragraph">
+                      <p>{row.content.paragraph}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class={(index % 2 ? "order-sm-2" : "") + " col-sm-5"}>
+                  <img
+                    className="img-responsive blur-bleed"
+                    src={row.image.url}
+                    alt={row.image.alt}
+                  />
+                </div>
+                <div
+                  className={(index % 2 ? "order-sm-1" : "") + " col-sm-1"}
+                />
+              </div>
+            ))}
+            {/* End Split Row of content and image */}
+          </Container>
+        </Main>
+      </div>
+    </DefaultPageLayout>
+  )
+}
