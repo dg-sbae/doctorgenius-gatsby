20bbaee1bd2c79a3ddd6befe54f4a104e1cbbc2a
Brent Robertson <brobertson@doctorgenius.com>
2019-05-13 18:02:35 -0700

    Passed in the props for each of the remaining inner pages

--- src/pages/our-clients/urgent-care-clinics.js
+++ src/pages/our-clients/urgent-care-clinics.js
@@ -1,10 +1,31 @@
 import React from "react"
 
-import DefaultPageLayout from "../../components/DefaultPageLayout"
-
+import OurClientsInnerPages from "../../templates/our-clients-inner-pages"
 
 export default () => (
-  <DefaultPageLayout>
-    <h1>Urgent Care Clinics</h1>
-  </DefaultPageLayout>
+  <OurClientsInnerPages
+    page="/our-clients/urgent-care-clinics/" /* this should be dynamic from the page props */
+    pageTitle="Urgent Care Clinics"
+    pageSubTitle="<span>Engage and Convert More</span> Patients To Your Practice"
+    pageIntro="Doctor Genius provides digital marketing solutions by leveraging content, mobile-first web design, SEO, online directories, and social media to drive new patients to your private practice"
+    contentRows={[
+      {
+        content: {
+          icon: {
+            url: "/icon/website.svg",
+            alt: "Blog and Social updates",
+            title: "Blog and Social updates",
+          },
+          innerTitle:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          paragraph:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+        },
+        image: {
+          url: "/png/design-your-presence.png",
+          alt: "Lorem Ipsum",
+        },
+      },
+    ]}
+  />
 )
