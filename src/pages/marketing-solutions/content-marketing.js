f804ea8c7ddc70ac1eb3a646ef08ba8523b7dc68
Brent Robertson <brobertson@doctorgenius.com>
2019-05-15 20:47:57 -0700

    Replaced most of the placeholders on the Content Marketing page.

--- src/pages/marketing-solutions/content-marketing.js
+++ src/pages/marketing-solutions/content-marketing.js
@@ -5,18 +5,20 @@ import MarketingSolutionsTemplate from "../../templates/marketing-solutions-inne
 export default () => (
   <MarketingSolutionsTemplate
     page="/marketing-solutions/content-marketing/" /* this should be dynamic from the page props */
+    pageTitle="Content Marketing"
+    pageSubtitle="<span>Build New</span> Organic Traffic"
+    pageIntro="Our organic content strategy main objective is to funnel traffic to your website."
     contentRows={[
       {
         content: {
           icon: {
             url: "/icon/website.svg",
-            alt: "Blog and Social updates",
-            title: "Blog and Social updates",
+            alt: "Blog and Social Updates",
+            title: "Blog and Social Updates",
           },
-          innerTitle:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          innerTitle: "Content is King.",
           paragraph:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+            "Google is an Ecosystem powered by information. Doctor Genius will write content related to your most profitable services and utilize additional techniques like Accelerated Mobile Pages(AMP).",
         },
         image: {
           url: "/png/design-your-presence.png",
@@ -30,10 +32,9 @@ export default () => (
             alt: "Patient Education",
             title: "Patient Education",
           },
-          innerTitle:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          innerTitle: "Understanding your practice.",
           paragraph:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+            "Original patient education content is posted to your blog and social media profiles to generate general interest in your medical treatments and services.",
         },
         image: {
           url: "/png/design-your-presence.png",
@@ -47,10 +48,9 @@ export default () => (
             alt: "Video Education",
             title: "Video Education",
           },
-          innerTitle:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          innerTitle: "Seeing is believing.",
           paragraph:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a placerat nunc, et venenatis felis. Sed in nunc suscipit, sodales lectus eget, pretium erat. Donec semper hendrerit mattis.",
+            "Strategic deployment of educational videos as an additional avenue for new patients to find you.",
         },
         image: {
           url: "/png/design-your-presence.png",
