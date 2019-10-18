22c1c21fc7c9693077e59534985ddccc0f055e6e
jyosas <jyosas@doctorgenius.com>
2019-10-18 16:32:37 -0700

    Footer styling complete.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,13 +33,29 @@ const ExternalLink = props => (
 )
 
 export default ({ children }) => (
-  <footer>
+  <footer className={FooterStyles.FooterStyling}>
     <Container>
-      <div className="row padded tall-top footer-row">
+      <div
+        className={
+          FooterStyles.paddedFooterRow +
+          " " +
+          FooterStyles.footerRow +
+          " row padded tall-top footer-row"
+        }
+      >
         <div className="col-sm-12 col-lg-4">
-          <img src={logoWhite} className="logo img-responsive" alt="Our Logo" />
+          <img
+            src={logoWhite}
+            className={FooterStyles.logo + " logo img-responsive"}
+            alt="Our Logo"
+          />
         </div>
-        <div className="d-none d-sm-none d-md-block col-sm-12 col-md-6 col-lg-4 footer-page-links">
+        <div
+          className={
+            FooterStyles.footerPageLinks +
+            " d-none d-sm-none d-md-block col-sm-12 col-md-6 col-lg-4 footer-page-links"
+          }
+        >
           <ul>
             <ListLink to="/">Home</ListLink>
             <ListLink to="/marketing-solutions">Marketing Solutions</ListLink>
@@ -57,58 +73,106 @@ export default ({ children }) => (
           </ul>
         </div>
         <div className="col-10 col-sm-10 col-md-6 col-lg-4">
-          <div className="footer-contact-row">
-            <div className="icon-wrapper">
+          <div
+            className={FooterStyles.footerContactRow + " footer-contact-row"}
+          >
+            <div
+              className={
+                FooterStyles.footerIconWrapper + " footer-icon-wrapper"
+              }
+            >
               <img
-                className="img-icon"
+                className={FooterStyles.imgIcon + " img-icon"}
                 src={ringingPhoneIcon}
                 alt="Phone Icon"
               />
             </div>
-            <p className="footer-contact-details">&#40;877&#41; 477-2311</p>
+            <p
+              className={
+                FooterStyles.footerContactDetails + " footer-contact-details"
+              }
+            >
+              &#40;877&#41; 477-2311
+            </p>
           </div>
-          <div className="footer-contact-row">
-            <div className="icon-wrapper">
+          <div
+            className={FooterStyles.footerContactRow + " footer-contact-row"}
+          >
+            <div
+              className={
+                FooterStyles.footerIconWrapper + " footer-icon-wrapper"
+              }
+            >
               <img
-                className="img-icon"
+                className={FooterStyles.imgIcon + " img-icon"}
                 src={envelopeIcon}
                 alt="Envelope Icon"
               />
             </div>
-            <p className="footer-contact-details">support@doctorgenius.com</p>
+            <p
+              className={
+                FooterStyles.footerContactDetails + " footer-contact-details"
+              }
+            >
+              support@doctorgenius.com
+            </p>
           </div>
-          <div className="footer-contact-row">
-            <div className="icon-wrapper">
-              <img className="img-icon" src={mapPin} alt="Map Icon" />
+          <div
+            className={FooterStyles.footerContactRow + " footer-contact-row"}
+          >
+            <div
+              className={
+                FooterStyles.footerIconWrapper + " footer-icon-wrapper"
+              }
+            >
+              <img
+                className={FooterStyles.imgIcon + " img-icon"}
+                src={mapPin}
+                alt="Map Icon"
+              />
             </div>
-            <address className="footer-contact-details">
+            <address
+              className={
+                FooterStyles.footerContactDetails + " footer-contact-details"
+              }
+            >
               2121 Alton Parkway
               <br />
               Irvine, CA 92606
             </address>
           </div>
-          <div className="social-media-row">
-            <div className="social-media-item">
+          <div className={FooterStyles.socialMediaRow + " social-media-row"}>
+            <div
+              className={FooterStyles.socialMediaItem + " social-media-item"}
+            >
               <a href="https://www.facebook.com/DoctorGeniusMarketing">
                 <img src={facebookIcon} alt="Facebook Icon" />
               </a>
             </div>
-            <div className="social-media-item">
+            <div
+              className={FooterStyles.socialMediaItem + " social-media-item"}
+            >
               <a href="https://twitter.com/DoctorGeniusCA">
                 <img src={twitterIcon} alt="Twitter Icon" />
               </a>
             </div>
-            <div className="social-media-item">
+            <div
+              className={FooterStyles.socialMediaItem + " social-media-item"}
+            >
               <a href="https://www.instagram.com/doctor.genius/">
                 <img src={instagramIcon} alt="Instagram Icon" />
               </a>
             </div>
-            <div className="social-media-item">
+            <div
+              className={FooterStyles.socialMediaItem + " social-media-item"}
+            >
               <a href="https://www.linkedin.com/company/doctor-genius">
                 <img src={linkedinIcon} alt="LinkedIn Icon" />
               </a>
             </div>
-            <div className="social-media-item">
+            <div
+              className={FooterStyles.socialMediaItem + " social-media-item"}
+            >
               <a href="https://www.youtube.com/channel/UCEOt77NoRiRrQzDgjpQwDCA/videos">
                 <img src={youtubeIcon} alt="Youtube Icon" />
               </a>
@@ -118,11 +182,11 @@ export default ({ children }) => (
       </div>
       <div className="row padded short-top">
         <div className="col-sm-12">
-          <div className="footer-subrow">
+          <div className={FooterStyles.footerSubrow + " footer-subrow"}>
             <div className="copyright-section">
               &copy;2019 Doctor Genius. All Rights Reserved.
             </div>
-            <div className="legal-links">
+            <div className={FooterStyles.legalLinks + " legal-links"}>
               <ListLink to="/privacy-policy">Privacy Policy</ListLink>
               {/* Waiting for page content before we show this link */}
               {/*<span> | </span>
