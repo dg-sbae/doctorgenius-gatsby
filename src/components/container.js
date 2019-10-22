da8532cad3f3c4cea8f87037b8ed7d09e348adf7
jyosas <jyosas@doctorgenius.com>
2019-10-22 11:12:16 -0700

    Bootstrap - global container styling added modularly.

--- src/components/container.js
+++ src/components/container.js
@@ -4,7 +4,9 @@ import containerStyles from "./container.module.scss"
 const bootstrapContainerClassName = "container-fluid"
 
 export default ({ children }) => (
-  <div className={(containerStyles.container, bootstrapContainerClassName)}>
+  <div
+    className={containerStyles.container + " " + bootstrapContainerClassName}
+  >
     {children}
   </div>
 )
