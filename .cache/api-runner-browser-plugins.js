0c17c6338da8175f0854fcc18b107896d63e3dab
dg-sbae <sbae@doctorgenius.com>
2021-03-22 15:37:42 -0700

    added other .files and lock files into tracked files

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-google-tagmanager/gatsby-browser.js'),
+      options: {"plugins":[],"id":"GTM-52SPGC","includeInDevelopment":true,"defaultDataLayer":null,"routeChangeEventName":"gatsby-route-change"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
+      options: {"plugins":[],"google":{"families":["Montserrat:400,700","Roboto:300,400,500&display=swap"]}},
+    }]
